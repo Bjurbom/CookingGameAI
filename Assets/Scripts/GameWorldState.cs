3318dc4536b95e97740498c9aba4a352b6d25498
Tor Bjurbom <34156187+Bjurbom@users.noreply.github.com>
2025-06-26 15:26:42 +0200

    Added machine for making the cake

--- Assets/Scripts/GameWorldState.cs
+++ Assets/Scripts/GameWorldState.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class GameWorldState : MonoBehaviour
 {
+    [SerializeField]
+    GameObject UIObject;
+
+
     public void DoneWithAnimaiton()
     {
         WorldState.animationDone = true;
+        UIObject.SetActive(true);
     }
+
+
+
+
 }
 
 

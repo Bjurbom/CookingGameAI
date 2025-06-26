10713ad770f03188821060298c52b62014de9397
Tor Bjurbom <34156187+Bjurbom@users.noreply.github.com>
2025-06-26 14:09:24 +0200

    Initial Commit two

---
+++ Assets/DestroyHelperScript.cs
@@ -0,0 +1,14 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyHelperScript : MonoBehaviour
+{
+    [SerializeField]
+    private string SceneToLoad = "GameScene";
+
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/CameraCheckerScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraCheckerScript : MonoBehaviour
+{
+    static CameraCheckerScript instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
